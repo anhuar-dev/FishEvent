6350479e64596f085e526ae84df837b1342d45f3
Anhuar Ruiz <myclxss@outlook.com>
2025-06-18 20:59:46 -0600

    create project

---
+++ src/main/java/dev/anhuar/fishEvent/FishEvent.java
@@ -0,0 +1,17 @@
+package dev.anhuar.fishEvent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FishEvent extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
